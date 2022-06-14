a78eab79a9bceb008f969fa54da1b7214ca504e3
Simon <saimeunt@gmail.com>
2022-06-14 22:45:39 +0200

    fix

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -1,13 +1,13 @@
-import { writeFileSync } from 'fs';
+// import { writeFileSync } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { google } from 'googleapis';
+// import { google } from 'googleapis';
 
 const callback = async (
   req: NextApiRequest & { url: string },
   res: NextApiResponse,
 ): Promise<void> => {
   try {
-    const qs = new URL(req.url, 'http://localhost:3000').searchParams;
+    /* const qs = new URL(req.url, 'http://localhost:3000').searchParams;
     const client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
       process.env.GOOGLE_CLIENT_SECRET,
@@ -15,7 +15,7 @@ const callback = async (
     );
     const { tokens } = await client.getToken(qs.get('code') as string);
     writeFileSync('config/tokens.json', JSON.stringify(tokens));
-    res.end('Saved tokens to config/tokens.json');
+    res.end('Saved tokens to config/tokens.json'); */
   } catch (error) {
     console.error(error);
     res.status(500).end();
--- pages/api/auth/store-tokens.ts
+++ pages/api/auth/store-tokens.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { google } from 'googleapis';
+// import { google } from 'googleapis';
 
 const storeTokens = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   try {
-    const client = new google.auth.OAuth2(
+    /* const client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
       process.env.GOOGLE_CLIENT_SECRET,
       process.env.GOOGLE_REDIRECT_URI,
@@ -12,7 +12,7 @@ const storeTokens = async (req: NextApiRequest, res: NextApiResponse): Promise<v
       access_type: 'offline',
       scope: ['https://www.googleapis.com/auth/gmail.send'],
     });
-    res.redirect(authUrl);
+    res.redirect(authUrl); */
   } catch (error) {
     console.error(error);
     res.status(500).end();
