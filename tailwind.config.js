d88faff647e8d8e9c28c5e0a0d125e9e5f6be436
Simon <saimeunt@gmail.com>
2023-03-28 21:36:47 +0200

    update tailwind

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')],
-};
